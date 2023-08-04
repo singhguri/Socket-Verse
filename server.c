5b063df12a3c3f6e60b74caae137e55bd825e7ca
singhguri <singhguri5598@gmail.com>
2023-08-03 23:41:11 -0400

    converted send/recv to read/write, added file_search functionality

--- server.c
+++ server.c
@@ -9,106 +9,180 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <dirent.h>
+#include <fcntl.h>
 
 #define BUFSIZE 1024
 #define _XOPEN_SOURCE 500
 #define PORT 8080
 #define MIRROR_PORT 7001
 
+struct
+{
+	char *file_name;
+	int file_size;
+	char *file_created_date;
+} File_info;
+
+int day, year, hour, min, sec;
+char month[4];
+
+void file_search(char *base_path, char *filename)
+{
+
+	// Searching for file in the root directory
+	char command_buf[BUFSIZE];
+	sprintf(command_buf, "find %s -maxdepth 1 -name %s -printf \"%%s,%%Tc\n\"", base_path, filename);
+	FILE *fp = popen(command_buf, "r");
+	char line[BUFSIZE];
+
+	// if File found in root directory
+	if (fgets(line, BUFSIZE, fp) != NULL)
+	{
+		File_info.file_name = strdup(filename);
+		File_info.file_size = atoi(strtok(line, ","));
+
+		char *date = strtok(NULL, ",");
+		if (sscanf(date, "%*s %s %d %d:%d:%d %d", month, &day, &hour, &min, &sec, &year) == 6)
+			sprintf(date, "%s %d, %d %d:%d:%d\n", month, day, year, hour, min, sec);
+		File_info.file_created_date = strdup(date);
+	}
+	else
+	{
+		// Search for the file in all subdirectories
+		sprintf(command_buf, "find %s -name %s -printf \"%%s,%%Tc\n\"", base_path, filename);
+		fp = popen(command_buf, "r");
+
+		// if File found in subdirectory
+		if (fgets(line, BUFSIZE, fp) != NULL)
+		{
+			File_info.file_name = strdup(filename);
+			File_info.file_size = atoi(strtok(line, ","));
+
+			char *date = strtok(NULL, ",");
+			if (sscanf(date, "%*s %s %d %d:%d:%d %d", month, &day, &hour, &min, &sec, &year) == 6)
+				sprintf(date, "%s %d, %d %d:%d:%d\n", month, day, year, hour, min, sec);
+			File_info.file_created_date = strdup(date);
+		}
+
+		// if File not found
+		else
+			File_info.file_size = -1;
+	}
+	pclose(fp);
+}
+
 // redirect to the mirror
 void redirect_to_mirror(int cli_fd)
 {
 	char redirect_msg[BUFSIZE];
 	snprintf(redirect_msg, BUFSIZE, "%d\n", MIRROR_PORT);
-	send(cli_fd, redirect_msg, strlen(redirect_msg), 0);
+	write(cli_fd, redirect_msg, strlen(redirect_msg));
+	// send(cli_fd, redirect_msg, strlen(redirect_msg), 0);
 	close(cli_fd);
 }
 
 void processclient(int skt_fd)
 {
-  char buff[BUFSIZE] = {0};
-  char command[BUFSIZE] = {0};
-  char gf[BUFSIZE] = {0};
+	char cmd[BUFSIZE] = {0};
+	char response[BUFSIZE * 2] = {0};
+	char gf[BUFSIZE] = {0};
 
-  while (1)
-  {
+	printf("New client connected. Client id: %d...\n", getpid());
 
-    memset(buff, 0, sizeof(buff));
-    memset(command, 0, sizeof(command));
-    memcpy(gf, buff, sizeof(buff));
+	while (1)
+	{
+		if (response != NULL && strcmp(response, "Goodbye") == 0)
+			break;
 
-    int valread = read(skt_fd, buff, sizeof(buff));
-    buff[valread] = '\0';
+		memset(cmd, 0, sizeof(cmd));
+		memset(response, 0, sizeof(response));
+		memcpy(gf, cmd, sizeof(cmd));
 
-    // Parse command
-    char *token = strtok(buff, " ");
-    // handle commands
-    if (token == NULL)
-      sprintf(command, "The syntax is Invalid. Please try again.\n");
-    else if (strcmp(token, "fgets") == 0)
-    {
-      // execcution of fgets command
-      char *filename = strtok(NULL, " ");
-      if (filename == NULL)
-        sprintf(command, "The syntax is Invalid. Please try again.\n");
-      else
-      {
-        // fgets logic
-      }
-    }
-    else if (strcmp(buff, "quit") == 0)
-    {
-      sprintf(command, "Goodbye.\n");
-      break;
-    }
-    else
-      sprintf(command, "The syntax is invalid. Please try again.\n");
+		int val_read = read(skt_fd, cmd, sizeof(cmd));
+		cmd[val_read] = '\0';
 
-    // Send response to client
-    send(skt_fd, command, strlen(command), 0);
-  }
+		// Parse command
+		char *token = strtok(cmd, " ");
+		// handle commands
+		if (token == NULL)
+			sprintf(response, "The syntax is Invalid. Please try again.\n");
+		else if (strcmp(token, "fgets") == 0)
+		{
+			// execcution of fgets command
+			char *filename = strtok(NULL, " ");
+			if (filename == NULL)
+				sprintf(response, "The syntax is Invalid. Please try again.\n");
+			else
+			{
+				// fgets logic
+			}
+		}
+		else if (strcmp(token, "filesrch") == 0)
+		{
+			char *filename = strtok(NULL, " ");
+			if (filename == NULL)
+				sprintf(response, "The syntax is Invalid. Please try again.\n");
+			else
+			{
+				// fsearch logic
+				File_info.file_size = 0;
+				// serach file by file name in $HOME directory
+				file_search("$HOME", filename);
 
-  close(skt_fd);
-  exit(EXIT_SUCCESS);
+				sprintf(response, "Name: %s\t\tSize: %d bytes\t\tCreated date: %s", File_info.file_name, File_info.file_size, File_info.file_created_date);
+
+				if (File_info.file_size == -1)
+					sprintf(response, "File not found.\n");
+			}
+		}
+		else if (strcmp(token, "quit") == 0)
+		{
+			printf("client %d exited...\n", getpid());
+			sprintf(response, "Goodbye\n");
+		}
+		else
+			sprintf(response, "The syntax is invalid. Please try again.\n");
+
+		// Send response to client
+		write(skt_fd, response, strlen(response));
+		// send(skt_fd, response, strlen(response), 0);
+	}
+
+	close(skt_fd);
+	exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char const *argv[])
 {
-	
-	
 
-  int serv_fd, new_skt;
-  struct sockaddr_in serv_addr, cli_addr;
-  int opt = 1;
-  int addrlen = sizeof(serv_addr);
-  int no_of_clients = 1;
+	int serv_fd, new_skt;
+	struct sockaddr_in serv_addr, cli_addr;
+	int opt = 1;
+	int addrlen = sizeof(serv_addr);
+	int no_of_clients = 1;
 
 	// Create Raw socket
-  // Create socket file descriptor
-  if ((serv_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-  {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
-  }
+	// Create socket file descriptor
+	if ((serv_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
-	
 	////////////////////// TODO: check this ////////////////////////
-	
-  // Attach socket to the port 8080
-  if (setsockopt(serv_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0)
-  {
-    perror("setsockopt");
-    exit(EXIT_FAILURE);
-  }
-  
-  ///////////////////////////////////////////////////////////
-  
+
+	// Attach socket to the port 8080
+	if (setsockopt(serv_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0)
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
 
 	/// Attributes for binding socket with IP and PORT
-  	memset(&serv_addr, 0, sizeof(serv_addr));
-  	serv_addr.sin_family = AF_INET;
-  	serv_addr.sin_addr.s_addr = INADDR_ANY;
-  	serv_addr.sin_port = htons(PORT);
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(PORT);
 
 	// Bind socket to the PORT
 	if (bind(serv_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
@@ -117,15 +191,13 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-
 	// listen to the socket
-	// queue of size 150 
+	// queue of size 150
 	if (listen(serv_fd, 150) < 0)
 	{
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-
 
 	// waitin for client
 	printf("Waiting for client...\n");
@@ -136,9 +208,7 @@ int main(int argc, char const *argv[])
 			perror("accept");
 			exit(EXIT_FAILURE);
 		}
-		
-		printf("New client is connected. Forking child process...\n");
-		
+
 		// load balancing from server to mirror
 		// if active clients less than =6 or is an odd no. after 12 connections
 		// to be handled by server
@@ -161,10 +231,9 @@ int main(int argc, char const *argv[])
 			{
 				// Parent process
 				close(new_skt);
-				
-				///////// TODO: check this //////////////
+
 				while (waitpid(-1, NULL, WNOHANG) > 0)
-				  ; // Clean up zombie processes
+					; // Clean up zombie processes
 			}
 		}
 		else
@@ -173,7 +242,7 @@ int main(int argc, char const *argv[])
 			printf("Redirecting to mirror\n");
 			redirect_to_mirror(new_skt);
 		}
-		
+
 		// increase counter for no of connections
 		no_of_clients++;
 	}
